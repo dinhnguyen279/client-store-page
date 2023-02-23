227b1d25c206432b490f899fea131e5108b5f53b
Nguyễn <tdinhnguyen279@gmail.com>
2023-02-23 08:37:26 +0700

    fix: :bug: add images for product

--- src/Share/img/Image.jsx
+++ src/Share/img/Image.jsx
@@ -9,6 +9,9 @@ import img2 from './cat-img-2.jpg'
 import img3 from './cat-img-3.jpg'
 import img4 from './cat-img-4.jpg'
 import userprofile from './userprofile.png'
+import categorybanner1 from "./categorybanner1.png"
+import categorybanner2 from "./categorybanner2.png"
+import categorybanner3 from "./categorybanner3.png"
 
 const Image = {
     banner: banner,
@@ -21,7 +24,10 @@ const Image = {
     img2: img2,
     img3: img3,
     img4: img4,
-    userprofile: userprofile
+    userprofile: userprofile,
+    categorybanner1: categorybanner1,
+    categorybanner2: categorybanner2,
+    categorybanner3: categorybanner3
 }
 
 export default Image
