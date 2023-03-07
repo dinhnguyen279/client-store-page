9df6b235995060f23d919637cd7296dccc2ea009
Nguyễn <tdinhnguyen279@gmail.com>
2023-03-07 21:49:01 +0700

    style: :lipstick: update ui header

--- src/Authentication/LogoutLink.jsx
+++ src/Authentication/LogoutLink.jsx
@@ -1,13 +1,23 @@
 import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
-
+import { FaUserPlus, FaUserAlt } from "react-icons/fa"
 function LogoutLink(props) {
     return (
-        <li className="nav-item">
-            <Link className="nav-link" to={`/signin`}>
-                <i className="fas fa-user-alt mr-2 text-gray"></i>Đăng nhập
-            </Link>
-        </li>
+        <>
+            <li className="nav-item">
+                <Link className="nav-link" to={`/signin`}>
+                    <FaUserAlt className='mr-2 text-gray' /> Đăng nhập
+                </Link>
+            </li>
+            <li>
+                {"/"}
+            </li>
+            <li className="nav-item">
+                <Link className="nav-link" to={`/signup`}>
+                    <FaUserPlus className='mr-2 text-gray' /> Đăng ký
+                </Link>
+            </li>
+        </>
     );
 }
 
