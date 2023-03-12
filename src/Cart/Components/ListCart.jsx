10cd76630904282f36b6a61a4c29399132898a62
Thuận <88768977+TDT1401@users.noreply.github.com>
2023-03-12 11:00:41 +0700

    update

--- src/Cart/Components/ListCart.jsx
+++ src/Cart/Components/ListCart.jsx
@@ -1,168 +1,138 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
-import { Card } from 'react-bootstrap';
-import Image from "../../Share/img/Image"
-import { AiFillCaretLeft, AiFillCaretRight } from 'react-icons/ai';
+import React, { useEffect, useState } from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import { Card } from "react-bootstrap";
+import Image from "../../Share/img/Image";
+import { AiFillCaretLeft, AiFillCaretRight } from "react-icons/ai";
+import axios from "axios";
+import { HOST } from "../../domain/host/host";
 
-ListCart.propTypes = {
-    listCart: PropTypes.array,
-    onDeleteCart: PropTypes.func,
-    onUpdateCount: PropTypes.func
-};
+// ListCart.propTypes = {
+//     listCart: PropTypes.array,
+//     onDeleteCart: PropTypes.func,
+//     onUpdateCount: PropTypes.func
+// };
 
-ListCart.defaultProps = {
-    listCart: [],
-    onDeleteCart: null,
-    onUpdateCount: null
-}
+// ListCart.defaultProps = {
+//     listCart: [],
+//     onDeleteCart: null,
+//     onUpdateCount: null
+// }
 
 function ListCart(props) {
+  const URL_getProductById = `${HOST}/product`;
 
-    const { listCart, onDeleteCart, onUpdateCount } = props
+  const getCart = props.getCart;
+  const onDeleteCart = props.onDeleteCart;
+  const onUpdateCount = props.onUpdateCount;
+  const idUser = props.idUser;
+  const quantity = props.quantity;
 
-    const listProducts = listCart.data
-    console.log('listProducts', listProducts);
+  const [dataProduct, setDataProduct] = useState([]);
 
-    const handlerChangeText = (e) => {
+  const handlerChangeText = (e) => {
+    console.log(e.target.value);
+  };
 
-        console.log(e.target.value)
+  useEffect(() => {
+    axios
+      .get(`${URL_getProductById}/${getCart}`)
+      .then((response) => setDataProduct(response.data))
+      .catch((error) => console.log(error));
+  }, []);
 
+  const handlerDelete = (getUser, getProduct) => {
+    if (!onDeleteCart) {
+      return;
     }
 
-    const handlerDelete = (getUser, getProduct) => {
+    onDeleteCart(getUser, getProduct);
+  };
 
-        if (!onDeleteCart) {
-            return
-        }
-
-        onDeleteCart(getUser, getProduct)
+  const handlerDown = (getIdUser, getIdProduct, getCount) => {
+    if (!onUpdateCount) {
+      return;
     }
 
-    const handlerDown = (getIdUser, getIdProduct, getCount) => {
-
-        if (!onUpdateCount) {
-            return
-        }
-
-        if (getCount === 1) {
-            return
-        }
-
-        //Trước khi trả dữ liệu về component cha thì phải thay đổi biến count
-        const updateCount = parseInt(getCount) - 1
-
-        onUpdateCount(getIdUser, getIdProduct, updateCount)
-
+    if (getCount === 1) {
+      return;
     }
 
-    const handlerUp = (getIdUser, getIdProduct, getCount) => {
+    //Trước khi trả dữ liệu về component cha thì phải thay đổi biến count
+    const updateCount = parseInt(getCount) - 1;
 
-        if (!onUpdateCount) {
-            return
-        }
+    onUpdateCount(getIdUser, getIdProduct, updateCount);
+  };
 
-        //Trước khi trả dữ liệu về component cha thì phải thay đổi biến count
-        const updateCount = parseInt(getCount) + 1
-
-        onUpdateCount(getIdUser, getIdProduct, updateCount)
-
+  const handlerUp = (getIdUser, getIdProduct, getCount) => {
+    if (!onUpdateCount) {
+      return;
     }
 
-    return (
-        <div className="mb-4">
-            {
-                listProducts && listProducts.map((value, index) => (
-                    <>
-                        <div className='row listcart-product position-relative' key={index}>
-                            <div className='col-md-3 mb-3'>
-                                <Link className="reset-anchor d-block animsition-link">
-                                    <Card.Img src={Image.product_1} />
-                                </Link>
-                            </div>
-                            <div className='col-md-6'>
-                                <Card.Title className='mb-3'>{value.nameProduct}</Card.Title>
-                                <Card.Subtitle className='mb-3'>Size {"S"}</Card.Subtitle>
-                                <div className="quantity">
-                                    <button className="dec-btn p-0"
-                                        style={{ cursor: 'pointer' }}
-                                        onClick={() => handlerDown(value.idUser, value.idProduct, value.count)} >
-                                        <AiFillCaretLeft />
-                                    </button>
-                                    <input
-                                        className="form-control form-control-sm border-0 shadow-0 p-0"
-                                        type="text"
-                                        value={value.count}
-                                        onChange={handlerChangeText} />
-                                    <button className="inc-btn p-0"
-                                        style={{ cursor: 'pointer' }}
-                                        onClick={() => handlerUp(value.idUser, value.idProduct, value.count)} >
-                                        <AiFillCaretRight />
-                                    </button>
-                                </div>
-                                <div className="align-middle border-0">
-                                    <p className="mb-0 small">{parseInt(value.priceProduct) * parseInt(value.count)},000₫</p>
-                                </div>
-                            </div>
-                            <div className='col-md-3 text-end'>
-                                <a href='/cart'
-                                    className="reset-anchor remove_cart"
-                                    style={{ cursor: 'pointer' }}
-                                    onClick={() => handlerDelete(value.idUser, value.idProduct)}>
-                                    <i className="fas fa-trash-alt text-muted"></i></a>
-                            </div>
-                            <div className='col-md-12 d-flex justify-content-between total-listcart'>
-                                <p style={{ fontWeight: "700" }}>Thành tiền:</p>
-                                <p className="mb-0 small" style={{ color: "red", fontWeight: "700" }}>{parseInt(value.priceProduct) * parseInt(value.count)},000₫</p>
-                            </div>
-                        </div>
-                    </>
+    //   Trước khi trả dữ liệu về component cha thì phải thay đổi biến count
+    const updateCount = parseInt(getCount) + 1;
 
-                    // {/*   // <tr className="text-center" key={index}>
+    onUpdateCount(getIdUser, getIdProduct, updateCount);
+  };
 
-                    //         //             <Link className="reset-anchor d-block animsition-link" to={`/detail/${value.idProduct}`}>
-                    //         //                 <img src={value.img} alt="..." width="70" />
-                    //         //             </Link>
-                    //         //         </div>
-                    //         //     
-                    //         //             <Link className="reset-anchor h6 animsition-link" to={`/detail/${value.idProduct}`}>
-                    //         //                 {value.nameProduct}
-                    //         //             </Link>
-
-                    //         //         <p className="mb-0 small">${value.priceProduct}</p>
-                    //         //                 style={{ cursor: 'pointer' }}
-                    //         //                 onClick={() => handlerDown(value.idUser, value.idProduct, value.count)} >
-                    //         //                 <i className="fas fa-caret-left"></i>
-                    //         //             </button>
-                    //         //             <input
-                    //         //                 className="form-control form-control-sm border-0 shadow-0 p-0"
-                    //         //                 type="text"
-                    //         //                 value={value.count}
-                    //         //                 onChange={handlerChangeText} />
-                    //         //             <button className="inc-btn p-0"
-                    //         //                 style={{ cursor: 'pointer' }}
-                    //         //                 onClick={() => handlerUp(value.idUser, value.idProduct, value.count)} >
-                    //         //                 <i className="fas fa-caret-right"></i>
-                    //         //             </button>
-                    //         //         </div>
-                    //         //     </td>
-                    //         //     <td className="align-middle border-0">
-                    //         //         <p className="mb-0 small">${parseInt(value.priceProduct) * parseInt(value.count)}</p>
-                    //         //     </td>
-                    //         //     <td className="align-middle border-0">
-                    //         //         <a href='/'
-                    //         //             className="reset-anchor remove_cart"
-                    //         //             style={{ cursor: 'pointer' }}
-                    //         //             onClick={() => handlerDelete(value.idUser, value.idProduct)}>
-                    //         //             <i className="fas fa-trash-alt small text-muted"></i></a>
-                    //         //     </td>
-                    //         // </tr>*/}
-                ))
-            }
-            {/* </tbody>
-            </table> */}
+  return (
+    <div className="mb-4">
+      <div className="row listcart-product position-relative">
+        <div className="col-md-3 mb-3">
+          <Link className="reset-anchor d-block animsition-link">
+            <Card.Img src={dataProduct.avt} />
+          </Link>
         </div>
-    );
+        <div className="col-md-6">
+          <Card.Title className="mb-3">{dataProduct.name}</Card.Title>
+          <Card.Subtitle className="mb-3">Size {"S"}</Card.Subtitle>
+          <div className="quantity">
+            <button
+              className="dec-btn p-0"
+              style={{ cursor: "pointer" }}
+              onClick={() => handlerDown(idUser, getCart, quantity)}
+            >
+              <AiFillCaretLeft />
+            </button>
+            <input
+              className="form-control form-control-sm border-0 shadow-0 p-0"
+              type="text"
+              value={quantity}
+              onChange={handlerChangeText}
+            />
+            <button
+              className="inc-btn p-0"
+              style={{ cursor: "pointer" }}
+              onClick={() => handlerUp(idUser, getCart, quantity)}
+            >
+              <AiFillCaretRight />
+            </button>
+          </div>
+          <div className="align-middle border-0">
+            <p className="mb-0 small">
+              {parseInt(dataProduct.price) * parseInt(1)}₫
+            </p>
+          </div>
+        </div>
+        <div className="col-md-3 text-end">
+          <a
+            href="/cart"
+            className="reset-anchor remove_cart"
+            style={{ cursor: "pointer" }}
+            onClick={() => handlerDelete(idUser, getCart)}
+          >
+            <i className="fas fa-trash-alt text-muted"></i>
+          </a>
+        </div>
+        <div className="col-md-12 d-flex justify-content-between total-listcart">
+          <p style={{ fontWeight: "700" }}>Thành tiền:</p>
+          <p className="mb-0 small" style={{ color: "red", fontWeight: "700" }}>
+            {parseInt(dataProduct.price) * parseInt(1)}₫
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ListCart;
