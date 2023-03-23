4f1aa00f07651082c4d3122da30294a1d3d0a7da
Thuận <88768977+TDT1401@users.noreply.github.com>
2023-03-23 20:58:08 +0700

    update footer

--- src/Share/Footer/Footer.jsx
+++ src/Share/Footer/Footer.jsx
@@ -18,20 +18,24 @@ function Footer(props) {
                     <div className="col-md-4 mb-3 mb-md-0">
                         <h6 className="text-uppercase mb-3">Thông tin liên hệ</h6>
                         <ul className="list-unstyled mb-0">
-                            <li><a className="footer-link" href="abcds">Gọi mua hàng:  0935 90 3666 (8h30 - 21h30)</a></li>
-                            <li><a className="footer-link" href="abcds">Gọi khiếu nại: 096 893 89 88 - 079 01 23456 (9h00 - 20h00)</a></li>
-                            <li><a className="footer-link" href="abcds">Gọi tư vấn:  0935 90 7979 (8h30 - 21h30)</a></li>
-                            <li><a className="footer-link" href="abcds">Gọi bảo hành: 0935 90 7979 - 079 01 23456 (10h00 - 20h00)</a></li>
+                            <li>Gọi mua hàng:  0935 90 3666 (8h30 - 21h30)</li>
+                            <li>Gọi khiếu nại: 096 893 89 88 - 079 01 23456 (9h00 - 20h00)</li>
+                            <li>Gọi tư vấn:  0935 90 7979 (8h30 - 21h30)</li>
+                            <li>Gọi bảo hành: 0935 90 7979 - 079 01 23456 (10h00 - 20h00)</li>
                         </ul>
                     </div>
                     <div className="col-md-4">
-                        <h6 className="text-uppercase mb-3">Sport Zone</h6>
+                        <h6 className="text-uppercase mb-3">Địa chỉ</h6>
                         <ul className="list-unstyled mb-0">
-                            <li><a className="footer-link" href="abcds">Địa chỉ: 55 Trần Văn Quang, Phường 10, Quận Tân Bình.</a></li>
-                            <li><a className="footer-link" href="abcds">Số điện thoại: 085 432 2789 - 093 596 8794</a></li>
-                            <li><a className="footer-link" href="abcds">Email: sportzone@gmail.com</a></li>
-                            <li><a className="footer-link" href="abcds">Thời gian mở của: 8:30 - 21:30</a></li>
+                            <li>Địa chỉ: 55 Trần Văn Quang, Phường 10, Quận Tân Bình.</li>
+                            <li>Số điện thoại: 085 432 2789 - 093 596 8794</li>
+                            <li>Email: sportzone@gmail.com</li>
+                            <li>Thời gian mở của: 8:30 - 21:30</li>
                         </ul>
+                    </div>
+                    <div className="col-md-4">
+                        <h6 className="text-uppercase mb-3">Về Sport Zone</h6>
+                        <p style={{color: "white"}}>Được thành lập từ 2023, Mang sứ mệnh đa dạng các sản phẩm chất lượng đến với môn Thể Thao Vua, <a style={{color: "white"}} href='/'>SPORT ZONE</a> cam kết uy tín và sự tín nhiệm cao nhất từ Khách hàng</p>
                     </div>
                 </div>
                 <div className="border-top pt-4" style={{ borderColor: '#1d1d1d !important' }}>
