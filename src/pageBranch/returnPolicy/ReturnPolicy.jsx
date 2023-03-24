e2dd30e5ee7efd5df9fbf600ee0d079141265d6c
Thuận <88768977+TDT1401@users.noreply.github.com>
2023-03-24 22:38:13 +0700

    update

--- src/pageBranch/returnPolicy/ReturnPolicy.jsx
+++ src/pageBranch/returnPolicy/ReturnPolicy.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
 
 const ReturnPolicy = () => {
   return (
     <div>
-      <h1>chinh sach doi tra</h1>
+      <h1>Chính sách đổi trả</h1>
+      <h2>1. Điều kiện đổi trả</h2>
+      <p>
+        Quý Khách hàng cần kiểm tra tình trạng hàng hóa và có thể đổi hàng/ trả
+        lại hàng ngay tại thời điểm giao/nhận hàng trong những trường hợp sau:
+        Hàng không đúng chủng loại, mẫu mã trong đơn hàng đã đặt hoặc như trên
+        website tại thời điểm đặt hàng. Không đủ số lượng, không đủ bộ như trong
+        đơn hàng. Tình trạng bên ngoài bị ảnh hưởng như rách bao bì, bong tróc,
+        bể vỡ… Khách hàng có trách nhiệm trình giấy tờ liên quan chứng minh sự
+        thiếu sót trên để hoàn thành việc hoàn trả/đổi trả hàng hóa.
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default ReturnPolicy
+export default ReturnPolicy;
