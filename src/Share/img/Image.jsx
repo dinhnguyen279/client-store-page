e69846e3dbd31ef666046dbe18f55379abd03164
Nguyễn <tdinhnguyen279@gmail.com>
2023-06-12 23:34:07 +0700

    docs: :memo: Downsizing size image

--- src/Share/img/Image.jsx
+++ src/Share/img/Image.jsx
@@ -1,34 +1,30 @@
-import banner from './banner.png'
-import banner1 from './banner1.png'
-import banner2 from './banner2.png'
-import banner3 from './banner3.png'
-import banner4 from './banner4.png'
-import banner5 from './banner5.png'
-import userprofile from './userprofile.png'
-import categorybanner1 from "./categorybanner1.png"
-import categorybanner2 from "./categorybanner2.png"
-import categorybanner3 from "./categorybanner3.png"
-import collection from "./collection.png"
-import home_collection_banner from "./home_collection_banner.png"
-import lookbooks_1 from "./lookbooks_1.png"
-import lookbooks_2 from "./lookbooks_2.png"
-import lookbooks_3 from "./lookbooks_3.png"
-import pro_featured from "./pro_featured.png"
-import home_groupbanner_banner1 from "./home_groupbanner_banner1.png"
-import home_groupbanner_banner2 from "./home_groupbanner_banner2.png"
-import product_under_banner from "./product_under_banner.png"
-import product_1 from "./product_under1.1.png"
-import product_1_1 from "./product_under1.png"
-import Carouselendpage1 from "./Carouselendpage1.png"
-import Carouselendpage2 from "./Carouselendpage2.png"
-import Carouselendpage3 from "./Carouselendpage3.png"
-import Carouselendpage4 from "./Carouselendpage4.png"
-import Carouselendpage5 from "./Carouselendpage5.png"
-import Carouselendpage6 from "./Carouselendpage6.png"
-import sizegiay from "./bangsizegiay.png"
-import logoPay from "./logoPay.png"
-import logoMomo from "./logoMomo.png"
-import avatarClone from "./default-avatar.png"
+import banner from './banner-min.jpg'
+import banner1 from './banner1-min.png'
+import banner2 from './banner2-min.png'
+import banner3 from './banner3-min.png'
+import banner4 from './banner4-min.png'
+import banner5 from './banner5-min.png'
+import categorybanner1 from "./categorybanner_1_img-min.png"
+import categorybanner2 from "./categorybanner_2-min.png"
+import categorybanner3 from "./categorybanner_3-min.png"
+import collection from "./collection_one_img-min.png"
+import home_collection_banner from "./home_collection_1_banner-min.png"
+import lookbooks_1 from "./lookbooks_1-min.png"
+import lookbooks_2 from "./lookbooks_2-min.png"
+import lookbooks_3 from "./lookbooks_3-min.png"
+import pro_featured from "./pro_featured-min.png"
+import home_groupbanner_banner1 from "./home_groupbanner_banner1-min.png"
+import home_groupbanner_banner2 from "./home_groupbanner_banner2-min.png"
+import product_under_banner from "./product_under_banner-min.png"
+import Carouselendpage1 from "./Carouselendpage1-min.png"
+import Carouselendpage2 from "./Carouselendpage2-min.png"
+import Carouselendpage3 from "./Carouselendpage3-min.png"
+import Carouselendpage4 from "./Carouselendpage4-min.png"
+import Carouselendpage5 from "./Carouselendpage5-min.png"
+import Carouselendpage6 from "./Carouselendpage6-min.png"
+import sizegiay from "./bangsizegiay-min.png"
+import logoPay from "./logoPay-min.png"
+import avatarClone from "./default-avatar-min.png"
 const Image = {
     banner: banner,
     banner1: banner1,
@@ -47,10 +43,7 @@ const Image = {
     pro_featured: pro_featured,
     home_groupbanner_banner1: home_groupbanner_banner1,
     home_groupbanner_banner2: home_groupbanner_banner2,
-    userprofile: userprofile,
     product_under_banner: product_under_banner,
-    product_1: product_1,
-    product_1_1: product_1_1,
     Carouselendpage1: Carouselendpage1,
     Carouselendpage2: Carouselendpage2,
     Carouselendpage3: Carouselendpage3,
@@ -59,7 +52,6 @@ const Image = {
     Carouselendpage6: Carouselendpage6,
     sizegiay: sizegiay,
     logoPay: logoPay,
-    logoMomo: logoMomo,
     avatarClone: avatarClone
 }
 export default Image
