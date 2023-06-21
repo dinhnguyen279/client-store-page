d9e1cb5bf5e49d1ec32593cc694130256b08c01b
Nguyễn <tdinhnguyen279@gmail.com>
2023-06-21 18:10:45 +0700

    fix: :bug: remove google auth

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { CountProvider } from './Context/CountContext'
-import { GoogleOAuthProvider } from '@react-oauth/google'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <GoogleOAuthProvider clientId='802254813565-8il0cldpmu6butm3ouq0jadvm78onjog.apps.googleusercontent.com'>
-    <React.StrictMode>
-      <CountProvider>
-        <App />
-      </CountProvider>
-    </React.StrictMode>
-  </GoogleOAuthProvider>
+  <React.StrictMode>
+    <CountProvider>
+      <App />
+    </CountProvider>
+  </React.StrictMode>
 )
